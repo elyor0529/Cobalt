06483e41fb088aeb2bc06dea7fbc51c6f706e2cd
Enigmatrix <enigmatrix2000@gmail.com>
2020-05-02 13:26:37 +0800

    add benchmark project

---
+++ tests/Cobalt.Tests.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Cobalt.Tests.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
