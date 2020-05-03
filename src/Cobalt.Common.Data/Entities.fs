d6a7e0c1b072eb634787fc7e25a1af994916192f
Enigmatrix <enigmatrix2000@gmail.com>
2020-05-03 14:06:08 +0800

    add entities

---
+++ src/Cobalt.Common.Data/Entities.fs
@@ -0,0 +1,85 @@
+namespace Cobalt.Common.Data.Entities
+
+open System
+
+
+type AppIdentification =
+    | Win32 of Win32
+    | UWP of UWP
+    | Java of Java
+and Win32 = { Path: string; }
+    with static member Id path = Win32 { Path = path }
+and UWP = { PRAID: string }
+    with static member Id praid = UWP { PRAID = praid }
+and Java = { MainJar: string }
+    with static member Id mainJar = Java { MainJar = mainJar }
+
+[<CLIMutable>]
+type App = {
+    Id: int64;
+    Name: string;
+    Identification: AppIdentification;
+    Background: string;
+    Icon: Lazy<byte[]>;
+    Tags: Lazy<Tag seq>;
+}
+and [<CLIMutable>] Tag = {
+    Id: int64;
+    Name: string;
+    Color: string;
+    Apps: Lazy<App seq>;
+}
+
+[<CLIMutable>]
+type Session = {
+    Id: int64;
+    Title: string;
+    CmdLine: string;
+    App: App;
+}
+
+[<CLIMutable>]
+type Usage = {
+    Id: int64;
+    Start: DateTime;
+    End: DateTime;
+    Session: Session;
+}
+
+type SystemEventKind = Logon | Logoff | Active | Idle
+
+[<CLIMutable>]
+type SystemEvent = {
+    Id: int64;
+    Timestamp: DateTime;
+    Kind: SystemEventKind;
+}
+
+type Target = App of App | Tag of Tag
+
+type TimeRange =
+    | Once of Once
+    | Repeated of Repeated
+and Once = {
+    Start: DateTime;
+    End:DateTime }
+    with static member TimeRange s e = Once { Start = s; End = e }
+and Repeated = {
+    StartOfDay: TimeSpan;
+    EndOfDay: TimeSpan;
+    Type: RepeatType }
+    with static member TimeRange s e t = Repeated { StartOfDay = s; EndOfDay = e; Type = t }
+and RepeatType = Daily | Weekly | Monthly
+
+type Reaction = 
+    | Kill
+    | Message of string
+
+[<CLIMutable>]
+type Alert = {
+    Id: int64;
+    Target: Target;
+    TimeRange: TimeRange;
+    Limit: TimeSpan;
+    Reaction: Reaction;
+}
---
+++ tests/Cobalt.Tests.Benchmarks/DataBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Cobalt.Common.Data.Entities;
+
+namespace Cobalt.Tests.Benchmarks
+{
+    public class DataBenchmarks
+    {
+        [Benchmark]
+        public void InsertApp()
+        {
+            var app = new App
+            {
+                Id = 0L,
+                Name = "Chrome",
+                Background = "#fefefe",
+                Icon = new Lazy<byte[]>(() => new byte[0]),
+                Identification = Win32.Id("C:\\Desktop\\dumb_file.txt")
+            };
+
+            var alert = new Alert
+            {
+                Id = 1L,
+                Limit = TimeSpan.FromHours(4),
+                Reaction = Reaction.NewMessage("pls no more web browsing"),
+                Target = Target.NewApp(app),
+                TimeRange = Repeated.TimeRange(TimeSpan.FromHours(08), TimeSpan.FromHours(18), RepeatType.Monthly)
+            };
+
+
+        }
+    }
+}
