4d23a56b5d9f3cc2a0ddf0932b51a4b517789962
Enigmatrix <enigmatrix2000@gmail.com>
2020-05-16 12:03:12 +0800

    remove Microsoft.Windows.Sdk.Contracts and gh build step

--- src/Cobalt.Engine/Infos/ProcessInfo.cs
+++ src/Cobalt.Engine/Infos/ProcessInfo.cs
@@ -74,43 +74,43 @@ namespace Cobalt.Engine.Infos
             return Id == other.Id;
         }
 
-        public async ValueTask<AppIdentification> GetIdentification()
+        public ValueTask<AppIdentification> GetIdentification()
         {
-            if (_appIdentification != null) return _appIdentification;
-            if (IsWinStoreApp)
-            {
-                var aumid = GetWindowsStoreAppUserModelId(Handle);
+            if (_appIdentification == null)
+                if (IsWinStoreApp)
+                {
+                    var aumid = GetWindowsStoreAppUserModelId(Handle);
 
-                /*
-                 * AppInfo.Find(aumid); // 19041 only
-                 */
+                    /*
+                     * AppInfo.Find(aumid); // 19041 only
+                     */
 
-                /*
-                var infos = await AppDiagnosticInfo.RequestInfoForAppAsync(aumid);
-                var info = infos[0].AppInfo;
-                */
+                    /*
+                    var infos = await AppDiagnosticInfo.RequestInfoForAppAsync(aumid);
+                    var info = infos[0].AppInfo;
+                    */
 
-                /*
-                Vanara.PInvoke.AdvApi32.OpenProcessToken(Handle, AdvApi32.TokenAccess.TOKEN_ALL_ACCESS, out var token);
-                uint len1 = 1024;
-                var buffer1 = new StringBuilder((int)len1);
-                Kernel32.GetPackageFullNameFromToken(token, ref len1, buffer1).ThrowIfFailed();
+                    /*
+                    Vanara.PInvoke.AdvApi32.OpenProcessToken(Handle, AdvApi32.TokenAccess.TOKEN_ALL_ACCESS, out var token);
+                    uint len1 = 1024;
+                    var buffer1 = new StringBuilder((int)len1);
+                    Kernel32.GetPackageFullNameFromToken(token, ref len1, buffer1).ThrowIfFailed();
+    
+    
+                    var p = new PackageManager();
+                    var package = p.FindPackage(buffer1.ToString());
+                    var apps = Task.Run(() => package.GetAppListEntriesAsync().AsTask()).Result;
+                    var appList = apps.ToList();
+                    var d = package.Dependencies.ToList();
+                    token.Dispose();*/
+                    _appIdentification = AppIdentification.NewUWP(aumid);
+                }
+                else
+                {
+                    _appIdentification = AppIdentification.NewWin32(GetPath(Handle));
+                }
 
-
-                var p = new PackageManager();
-                var package = p.FindPackage(buffer1.ToString());
-                var apps = Task.Run(() => package.GetAppListEntriesAsync().AsTask()).Result;
-                var appList = apps.ToList();
-                var d = package.Dependencies.ToList();
-                token.Dispose();*/
-                _appIdentification = AppIdentification.NewUWP(aumid);
-            }
-            else
-            {
-                _appIdentification = AppIdentification.NewWin32(GetPath(Handle));
-            }
-
-            return _appIdentification;
+            return new ValueTask<AppIdentification>(_appIdentification);
         }
 
         public static string GetWindowsStoreAppUserModelId(Kernel32.SafeHPROCESS handle)
