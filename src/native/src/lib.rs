417f07bc0c5cdc92020d2356f9da3c1409994343
Enigmatrix <enigmatrix2000@gmail.com>
2020-06-21 11:45:07 +0800

    add fundumental native types

--- src/native/src/lib.rs
+++ src/native/src/lib.rs
@@ -1,16 +1,8 @@
 #![feature(option_zip)]
+#![feature(trivial_bounds)]
 #![feature(try_trait)]
 #![feature(osstring_ascii)]
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
 mod util;
-mod info;
-mod watchers;
-
-use crate::util::*;
-
-#[no_mangle]
-pub unsafe fn add() -> FfiResult<u32> {
-    FfiResult::Err(Error { code: 05, cause: String::from("what") })
-}
--- src/native/src/util.rs
+++ src/native/src/util.rs
@@ -1,100 +1,72 @@
-#[macro_use]
-pub type FfiString = Vec<u16>;
+use safer_ffi::prelude::*;
+use widestring::*;
+use std::*;
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
-pub struct Ptr<T>(pub T);
-unsafe impl<T> Send for Ptr<T> {}
+pub struct FnPtr<T>(extern "cdecl" fn(T));
+impl<T> fmt::Debug for FnPtr<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:p}", &self.0)
+    }
+}
 
-#[repr(C, usize)]
+#[repr(C, u8)]
 #[derive(Debug)]
-pub enum FfiResult<T> {
+pub enum Error {
+    Win32(u32),
+    Custom(WideString)
+}
+
+#[repr(C, u8)]
+#[derive(Debug)]
+pub enum Result<T> {
     Ok(T),
     Err(Error)
 }
 
-#[repr(C)]
+#[repr(C, u8)]
 #[derive(Debug)]
-pub struct Error {
-    pub code: i32,
-    pub cause: String
+pub enum Nullable<T> {
+    Value(T),
+    Null
+}
+impl<T> From<Option<T>> for Nullable<T> {
+    fn from(opt: Option<T>) -> Self {
+        match opt {
+            Some(x) => Nullable::Value(x),
+            None => Nullable::Null
+        }
+    }
 }
 
+#[derive_ReprC]
 #[repr(C)]
 pub struct Subscription<T> {
     pub on_next: extern "cdecl" fn(&T),
     pub on_error: extern "cdecl" fn(Error),
-    pub on_complete: extern "cdecl" fn(),
+    pub on_complete: extern "cdecl" fn(()),
 }
-impl<T> std::fmt::Debug for Subscription<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Subscription").finish()
+impl<T> fmt::Debug for Subscription<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Subscription")
+            .field("on_next", &FnPtr(self.on_next))
+            .field("on_error", &FnPtr(self.on_error))
+            .field("on_complete", &FnPtr(self.on_complete))
+            .finish()
     }
 }
 
-impl<T> FfiResult<T> {
-    pub fn get_last_err() -> FfiResult<T> {
-        let err = std::io::Error::last_os_error();
-        FfiResult::Err(Error { code: err.raw_os_error().unwrap(), cause: err.to_string() })
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    extern "cdecl" fn sub_on_next(_: &u32) {}
+    extern "cdecl" fn sub_on_error(_: Error) {}
+    extern "cdecl" fn sub_on_compl(_: ()) {}
+
+    #[test]
+    fn subscription_debug() {
+        let sub = Subscription { on_next: sub_on_next, on_error: sub_on_error, on_complete: sub_on_compl };
+        let re = regex::Regex::new(r"Subscription \{ on_next: 0x[[:xdigit:]]+, on_error: 0x[[:xdigit:]]+, on_complete: 0x[[:xdigit:]]+ \}").unwrap();
+        assert!(re.is_match(format!("{:?}", sub).as_str()));
     }
-}
-
-impl<T> std::ops::Try for FfiResult<T>  {
-    type Ok = T;
-    type Error = Error;
-
-    fn into_result(self) -> Result<T, Error> {
-        match self {
-            FfiResult::Ok(x) => Ok(x),
-            FfiResult::Err(e) => Err(e)
-        }
-    }
-
-    fn from_ok(v: T) -> Self {
-        FfiResult::Ok(v)
-    }
-
-    fn from_error(e: Error) -> Self {
-        FfiResult::Err(e)
-    }
-}
-
-macro_rules! valid_bool {
-    ($e: expr) => {{
-        if $e { FfiResult::Ok(()) } else { FfiResult::get_last_err() }
-    }};
-}
-
-macro_rules! read_unicode_string {
-    ($p: expr, $str: expr) => {{
-        let mut buf_len = $str.Length as usize / 2;
-        let mut buf = vec![0u16; buf_len];
-        let res = winapi::um::memoryapi::ReadProcessMemory($p,
-            $str.Buffer as *mut _ as *mut winapi::ctypes::c_void,
-            buf.as_mut_ptr() as *mut _ as *mut winapi::ctypes::c_void,
-            buf_len * 2,
-            &mut buf_len
-        );
-        if res == 0 { panic_win32!() };
-        buf
-    }};
-}
-
-macro_rules! read_struct {
-    ($p: expr, $addr: expr, $typ: ty) => {{
-        let mut ret: $typ = std::mem::zeroed();
-        let res = winapi::um::memoryapi::ReadProcessMemory($p,
-            $addr as *mut _ as *mut winapi::ctypes::c_void,
-            &mut ret as *mut _ as *mut winapi::ctypes::c_void,
-            std::mem::size_of::<$typ>(),
-            std::ptr::null_mut()
-        );
-        if res == 0 { panic_win32!() };
-        ret
-    }};
-}
-
-macro_rules! panic_win32 {
-    () => {{
-        panic!(std::io::Error::last_os_error().to_string())
-    }}
 }
